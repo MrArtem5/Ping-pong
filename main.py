b23ef1fbd14f3cd1bba0a526ddccbf895a4b1345
MrArtem <artali3010@gmail.com>
2026-05-18 20:32:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from pygame import*
+from pygame import *
 import time as t
 
-W, H = 1000, 700
+font.init()
+font = font.Font(None, 32)
+pl_win1 = font.render("Игрок 2 победил!", True, (250, 250, 250))
+pl_win2 = font.render("Игрок 1 победил!", True, (250, 250, 250))
+
+W, H = 850, 650
 win = display.set_mode((W, H))
 display.set_caption('Ping-pong')
 back = (200, 255, 255)
-win.fill(back)
 
 class GameSprite(sprite.Sprite):
     def __init__(self, x, y, width, height, spr_img, speed):
@@ -23,35 +27,72 @@ class Player(GameSprite):
     def __init__(self, x, y, width, height, spr_img, speed):
         super().__init__(x, y, width, height, spr_img, speed)
     
-    def update_r(self):
-        keys = key.get_pressed()
-        if keys[K_w] and self.rect.x > 5:
-            self.rect.y -= self.speed
-        if keys[K_s] and self.rect.right < H - 5:
-            self.rect.y += self.speed
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.x > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.right < H - 5:
+        if keys[K_s] and self.rect.y < H - self.rect.height - 5:
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < H - self.rect.height - 5:
             self.rect.y += self.speed
         
-#class Enemy(GameSprite):
-#    def update(self):
-#        global max_lose
-#        global lost
-#        self.rect.y += self.speed
-#        if self.rect.y > H:
-#            self.rect.y = 0
-#            self.rect.x = randint(80, W - 80)
+class Enemy(GameSprite):
+    def update(self, *args, **kwargs):
+        return super().update(*args, **kwargs)
 
+player_r = Player(W - 65, H / 2 - 75, 50, 150, "roketka.jpg", 5)
+player_l = Player(15, H / 2 - 75, 50, 150, "roketka.jpg", 5)
+ball = Enemy(W / 2 - 25, H / 2 - 25, 50, 50, "ball.jpg", 0)
+
+finish = False
+lost = 0
 clock = time.Clock()
 run = True
 FPS = 80
+game_over = False
+
+speed_x = 3
+speed_y = 3
 
 while run:
     for e in event.get():
-        if e == QUIT:
-            run == False
+        if e.type == QUIT:
+            run = False
+            
+    if finish != True:
+        win.fill(back)
+        
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        
+        if sprite.collide_rect(player_r, ball) or sprite.collide_rect(player_l, ball):
+            speed_x *= -1
+            
+        if ball.rect.y > H - 50 or ball.rect.y < 0:
+            speed_y *= -1
+            
+        player_r.update_r()
+        player_l.update_l()
+        ball.update()
+        
+        player_r.reset()
+        player_l.reset()
+        ball.reset()
+        
+        if ball.rect.x < 0:
+            finish = True
+            win.blit(pl_win1, (W / 2 - 100, H / 2))
+            game_over = True
+            
+        if ball.rect.x > W - 50:
+            finish = True
+            win.blit(pl_win2, (W / 2 - 100, H / 2))
+            game_over = True
+            
     display.update()
     clock.tick(FPS)
